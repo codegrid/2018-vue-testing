1c63703ba26531a06bb04867cca04e3f93e46f58
nakajmg <nakajima.jmg@gmail.com>
2018-03-13 17:51:05 +0900

    3

--- src/components/UsersListItem.test.js
+++ src/components/UsersListItem.test.js
@@ -41,4 +41,124 @@ describe('UsersListItem.vue', () => {
     })
   })
 
+  describe('computed', () => {
+    describe('fullName', () => {
+      test('lastNameとfirstNameを連結した値が返ってくること', () => {
+        wrapper.setProps({
+          firstName: 'Tarou',
+          lastName: 'Yamada',
+        })
+        expect(wrapper.vm.fullName).toBe('Tarou Yamada')
+      })
+
+      test('値がStringであること', () => {
+        expect(typeof wrapper.vm.fullName).toBe('string')
+      })
+    })
+
+    describe('isMale', () => {
+      test('genderがmaleのときtrueであること', () => {
+        wrapper.setProps({
+          gender: 'male',
+        })
+        expect(wrapper.vm.isMale).toBe(true)
+      })
+
+      test('genderがmaleではないときにfalseであること', () => {
+        wrapper.setProps({
+          gender: 'female',
+        })
+        expect(wrapper.vm.isMale).toBe(false)
+      })
+
+      test('値がBooleanであること', () => {
+        expect(typeof wrapper.vm.isMale).toBe('boolean')
+      })
+    })
+
+    describe('isFemale', () => {
+      test('genderがfemaleのときtrueであること', () => {
+        wrapper.setProps({
+          gender: 'female',
+        })
+        expect(wrapper.vm.isFemale).toBe(true)
+      })
+
+      test('genderがfemaleではないときにfalseであること', () => {
+        wrapper.setProps({
+          gender: 'male',
+        })
+        expect(wrapper.vm.isFemale).toBe(false)
+      })
+
+      test('値がBooleanであること', () => {
+        expect(typeof wrapper.vm.isFemale).toBe('boolean')
+      })
+    })
+  })
+
+  describe('template', () => {
+    describe('v-on', () => {
+      test('ルート要素のclickでonClickメソッドが実行されること', () => {
+        const stub = jest.fn()
+        wrapper.setMethods({
+          onClick: stub,
+        })
+        wrapper.trigger('click')
+        expect(stub).toHaveBeenCalled()
+      })
+
+      test('removeButtonのclickでonClickRemoveメソッドが実行されること', () => {
+        const stub = jest.fn()
+        wrapper.setMethods({
+          onClickRemove: stub,
+        })
+        const removeButton = wrapper.find('.UsersListItem_RemoveButton')
+        removeButton.trigger('click')
+        expect(stub).toHaveBeenCalled()
+      })
+    })
+
+    describe(':class', () => {
+      test('genderがmaleのとき_maleクラスが付与されていること', () => {
+        wrapper.setProps({
+          gender: 'male',
+        })
+        expect(wrapper.classes()).toContain('_male')
+      })
+
+      test('genderがfemaleのとき_femaleクラスが付与されていること', () => {
+        wrapper.setProps({
+          gender: 'female',
+        })
+        expect(wrapper.classes()).toContain('_female')
+      })
+    })
+  })
+
+  describe('methods', () => {
+    describe('onClick', () => {
+      test('selectイベントがemitされること', () => {
+        wrapper.vm.onClick()
+        expect(wrapper.emitted('select')).not.toBeUndefined()
+      })
+
+      test('selectイベントのemitでidを含むオブジェクトが渡されること', () => {
+        wrapper.vm.onClick()
+        expect(wrapper.emitted('select')[0][0]).toEqual({id: 0})
+      })
+    })
+
+    describe('onClickRemove', () => {
+      test('removeイベントがemitされること', () => {
+        wrapper.vm.onClickRemove()
+        expect(wrapper.emitted('remove')).not.toBeUndefined()
+      })
+
+      test('removeイベントのemitでidを含むオブジェクトが渡されること', () => {
+        wrapper.vm.onClickRemove()
+        expect(wrapper.emitted('remove')[0][0]).toEqual({id: 0})
+      })
+    })
+  })
 })
